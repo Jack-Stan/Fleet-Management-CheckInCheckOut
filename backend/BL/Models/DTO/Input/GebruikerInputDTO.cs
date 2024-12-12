f3269d72cb7ea256691aa3338e6bf232aba478b3
RufinDeS <rufin.deschreye@student.hogent.be>
2024-12-12 18:37:53 +0100

    Toegevoegd: GebruikerInputDTO en GebruikerOutputDTO

---
+++ backend/BL/Models/DTO/Input/GebruikerInputDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Models.DTO.Input {
+    public class GebruikerInputDTO {
+        public string Email { get; set; }
+        public string Wachtwoord { get; set; }
+    }
+}
+
