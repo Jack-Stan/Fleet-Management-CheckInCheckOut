5b4b4044a850c5dd626a43dde61462ea93d4e803
RufinDeS <rufin.deschreye@student.hogent.be>
2024-12-12 18:40:26 +0100

    Aangepast: Configuratiebestanden voor Gebruiker en nieuwe migraties

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -3,14 +3,17 @@ using BL.Interfaces;
 using BL.Services;
 using DL.Data;
 using DL.Repositories;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Azure;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using System.Threading.RateLimiting;
 
 internal class Program {
-    private static void Main(string[] args) 
-    {
+    private static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
 
         #region Services
@@ -26,12 +29,17 @@ internal class Program {
         builder.Services.AddScoped<ReserveringService>();
         builder.Services.AddScoped<IReserveringRepository, ReserveringRepository>();
         builder.Services.AddScoped<StorageService>();
+        builder.Services.AddScoped<AuthService>();
+        builder.Services.AddScoped<GebruikerService>();
+        builder.Services.AddScoped<IGebruikerRepository, GebruikerRepository>();
+
 
         #endregion
 
         #region Databank config
 
-        builder.Services.AddDbContext<FleetManagementDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Docker")));
+        builder.Services.AddDbContext<FleetManagementDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("Docker")));
 
         #endregion
 
@@ -43,11 +51,9 @@ internal class Program {
 
         #region Azure storage config
 
-        builder.Services.AddAzureClients(options =>
-        {
-            // can't add this due to Github secrets
+        builder.Services.AddAzureClients(options => {
             options.AddBlobServiceClient(builder.Configuration.GetConnectionString("Storage"));
-        }); 
+        });
 
         #endregion
 
@@ -55,11 +61,9 @@ internal class Program {
 
         var policyName = "AllowReactFrontend";
 
-        builder.Services.AddCors(options =>
-        {
+        builder.Services.AddCors(options => {
             options.AddPolicy(name: policyName,
-                builder =>
-                {
+                builder => {
                     builder
                     .WithOrigins("http://localhost:3000")
                     .AllowAnyMethod()
@@ -71,34 +75,48 @@ internal class Program {
 
         #region Rate Limiter
 
-        builder.Services.AddRateLimiter(options =>
-        {
+        builder.Services.AddRateLimiter(options => {
             options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpcontext => RateLimitPartition.GetFixedWindowLimiter(
                 partitionKey: httpcontext.User.Identity?.Name ?? httpcontext.Request.Headers.Host.ToString(),
-                factory: partition => new FixedWindowRateLimiterOptions
-                {
+                factory: partition => new FixedWindowRateLimiterOptions {
                     AutoReplenishment = true,
                     PermitLimit = 10,
                     QueueLimit = 0,
                     Window = TimeSpan.FromSeconds(1)
                 }));
             options.RejectionStatusCode = 429;
-            options.OnRejected = async (context, token) =>
-            {
+            options.OnRejected = async (context, token) => {
                 context.HttpContext.Response.StatusCode = 429;
-                if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
-                {
+                if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter)) {
                     context.HttpContext.Response.Headers["Retry-After"] = retryAfter.ToString();
                     await context.HttpContext.Response.WriteAsync(
                         $"Too many requests", cancellationToken: token);
-                }
-                else
-                {
+                } else {
                     await context.HttpContext.Response.WriteAsync(
                         $"Too many requests");
                 }
             };
         });
+
+        #endregion
+
+        #region JWT Config
+
+        var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => {
+                options.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]))
+                };
+            });
+
+        builder.Services.AddAuthorization();
 
         #endregion
 
@@ -108,28 +126,21 @@ internal class Program {
 
         var headers = new Dictionary<string, string>()
         {
-                {"X-Frame-Options", "DENY" },
-                {"X-Xss-Protection", "1; mode=block"},
-                {"X-Content-Type-Options", "nosniff"},
-                {"Referrer-Policy", "no-referrer"},
-                {"X-Permitted-Cross-Domain-Policies", "none"},
-                {"Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()"},
-                /*{"Content-Security-Policy", builder.Environment.IsDevelopment() ?
-                    "default-src 'self' http://localhost:3000" : 
-                    }*/
+            {"X-Frame-Options", "DENY" },
+            {"X-Xss-Protection", "1; mode=block"},
+            {"X-Content-Type-Options", "nosniff"},
+            {"Referrer-Policy", "no-referrer"},
+            {"X-Permitted-Cross-Domain-Policies", "none"},
+            {"Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()"},
         };
 
-        if (!builder.Environment.IsDevelopment())
-        {
+        if (!builder.Environment.IsDevelopment()) {
             app.UseHsts();
         }
 
-        app.Use(async (context, next) =>
-        {
-            foreach (var keyValue in headers)
-            {
-                if (!context.Response.Headers.ContainsKey(keyValue.Key))
-                {
+        app.Use(async (context, next) => {
+            foreach (var keyValue in headers) {
+                if (!context.Response.Headers.ContainsKey(keyValue.Key)) {
                     context.Response.Headers.Append(keyValue.Key, keyValue.Value);
                 }
             }
@@ -140,18 +151,15 @@ internal class Program {
         #endregion
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-            {
+        if (app.Environment.IsDevelopment()) {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
 
         app.UseHttpsRedirection();
-
+        app.UseAuthentication();
         app.UseAuthorization();
-
         app.UseRateLimiter();
-
         app.UseCors(policyName);
 
         app.MapControllers();
