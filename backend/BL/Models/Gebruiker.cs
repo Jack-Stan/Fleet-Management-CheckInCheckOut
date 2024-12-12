71ddf2bb893bf9d6a1d714da0efc3564f0794869
RufinDeS <rufin.deschreye@student.hogent.be>
2024-12-12 18:38:08 +0100

    Toegevoegd: Gebruiker model

---
+++ backend/BL/Models/Gebruiker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BL.Models {
+    public class Gebruiker {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string WachtwoordHash { get; set; } = string.Empty;
+
+        public Gebruiker(string email, string wachtwoordHash) {
+            Email = email;
+            WachtwoordHash = wachtwoordHash;
+        }
+
+        public Gebruiker() { }
+    }
+}
